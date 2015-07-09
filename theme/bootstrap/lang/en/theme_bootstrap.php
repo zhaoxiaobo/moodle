d4cd28aa8ae9439fe7645ae600f7cd5aa0054eec
ouyangyu <ouyangyu_62@163.com>
2015-07-09 09:37:07 +0800

    bootstrap theme

---
+++ theme/bootstrap/lang/en/theme_bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+$string['pluginname'] = '方正';
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['fullscreen'] = 'Full screen';
+$string['closefullscreen'] = 'Close full screen';
+
+$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot"><h2>方正</h2>
+<img class=img-polaroid src="bootstrap/pix/screenshot.jpg" />
+<h3>关于</h3>
+<p>方正是一个应用Bootstrap框架实现的响应式Moodle主题。</p>
+<p>
+<h3>调整</h3>
+<p>
+本主题基于Moodle核心的基本主题和画布主题构建。如果您想修改它，我们建议您在修改之前先复制一份并换名。这样将来Moodle升级时才不会覆盖您定制的主题。而且一旦您搞乱了它，还有原始文件的备份。关于修改主题的更多信息请浏览Moodle文档。
+</p>
+<h3>说明</h3>
+<p>本主题由方正的安超编写和维护。可以通过anchao@founder.com.cn联系他。</p>
+<p>';
+
---
+++ theme/bootstrap/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2016061803;
+$plugin->requires  = 2013110500;
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = 2014120803;
+$plugin->component = 'theme_bootstrap';
