3d8c05aaef51f411732dd247e0483debb81ce790
zhaoxiaobo <zhaoxiaobo987@163.com>
2015-07-10 10:44:53 +0800

    next ceshi zhaoxiaobo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // Thisnihao file is part of Moodle - http://moodle.org/
 //
-// founder1Moodle is free software: you can redistribute it and/or modify
+// founder2Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
