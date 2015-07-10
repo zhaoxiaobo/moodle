def6102082c350be008e49738283be05f813417b
ouyangyu <ouyangyu_62@163.com>
2015-07-10 14:06:14 +0800

    version add local

--- index.php
+++ index.php
@@ -2,22 +2,6 @@
 
 // Thisnihao file is part of Moodle - http://moodle.org/
 //
-<<<<<<< HEAD
-// founder4Moodle is free software: you can redistribute it and/or modify
-=======
-// founder2Moodle is free software: you can redistribute it and/or modify
->>>>>>> 3d8c05aaef51f411732dd247e0483debb81ce790
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Founder Moodle frontpage.
