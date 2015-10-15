1d11cb0251f668afc23fd43c6b518dc5bf5e41a1
ouyangyu <ouyangyu_62@163.com>
2015-10-15 13:38:30 +0800

    user

--- user/lib.php
+++ user/lib.php
@@ -299,9 +299,12 @@ function user_get_user_details($user, $course = null, array $userfields = array(
     $userdetails = array();
     $userdetails['id'] = $user->id;
 
-    if (($isadmin or $currentuser) and in_array('username', $userfields)) {
+    //ouyangyu
+    /*if (($isadmin or $currentuser) and in_array('username', $userfields)) {
         $userdetails['username'] = $user->username;
-    }
+    }*/
+    $userdetails['username'] = $user->username;
+
     if ($isadmin or $canviewfullnames) {
         if (in_array('firstname', $userfields)) {
             $userdetails['firstname'] = $user->firstname;
